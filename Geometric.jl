484b3f03c14a1e58eb877b3f6bb82cc6982910e2
Tyler Parker <computemachines@gmail.com>
2017-02-28 12:46:57 -0500

    Initial Commit

---
+++ Geometric.jl
@@ -0,0 +1,49 @@
+module Geometric
+
+export Up, Down
+
+Up(x, y) = Up2(x, y)
+Up(x, y, z) = Up3(x, y, z)
+Up(x, y, z, t) = Up4(x, y, z, t)
+
+immutable Up2{A, B}
+    x :: A
+    y :: B
+end
+
+immutable Up3{A, B, C}
+    x :: A
+    y :: B
+    z :: C
+end
+
+immutable Up4{A, B, C, D}
+    x :: A
+    y :: B
+    z :: C
+    t :: D
+end
+
+immutable Down2{A, B}
+    x :: A
+    y :: B
+end
+
+Down(x, y) = Down2(x, y)
+Down(x, y, z) = Down3(x, y, z)
+Down(x, y, z, t) = Down4(x, y, z, t)
+
+immutable Down3{A, B, C}
+    x :: A
+    y :: B
+    z :: C
+end
+
+immutable Down4{A, B, C, D}
+    x :: A
+    y :: B
+    z :: C
+    t :: D
+end
+
+end
---
+++ Symbolic.jl
@@ -0,0 +1,22 @@
+module Symbolic
+import Base: +, -, *, /, ^, sin, cos, exp, log, sqrt
+
++(x::Union{Symbol, Expr}) = x
++(a::Union{Symbol, Expr, Number}, b::Union{Symbol, Expr, Number}) = :($a + $b)
+-(x::Union{Symbol, Expr}) = :(-$x)
+-(a::Union{Symbol, Expr, Number}, b::Union{Symbol, Expr, Number}) = a + -b
+
+*(a::Union{Symbol, Expr, Number}, b::Union{Symbol, Expr, Number}) = :($a * $b)
+/(n::Union{Symbol, Expr, Number}, d::Union{Symbol, Expr, Number}) = :($n / $d)
+
+^(x::Number, p::Union{Symbol, Expr}) = :($x ^ $p)
+^(x::Union{Symbol, Expr}, p::AbstractFloat) = :($x ^ $p)
+^(x::Union{Symbol, Expr}, p::Union{Symbol, Expr}) = :($x ^ $p)
+
+sin(x::Union{Symbol, Expr, Number}) = :(sin($x))
+cos(x::Union{Symbol, Expr, Number}) = :(cos($x))
+exp(x::Union{Symbol, Expr, Number}) = :(exp($x))
+log(x::Union{Symbol, Expr, Number}) = :(log($x))
+sqrt(x::Union{Symbol, Expr, Number}) = :(sqrt($x))
+
+end
