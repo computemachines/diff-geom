0ee321ef28b19452bc011c6074012eff4f0d2dcc
Tyler Parker <computemachines@gmail.com>
2017-03-04 14:00:11 -0500

    Add + and - operators on Ups and Downs

--- Geometric.jl
+++ Geometric.jl
@@ -1,6 +1,6 @@
 module Geometric
-import Base: getindex
-export Up, Down, getindex
+import Base: getindex, +, -
+export Up, Down
 
 Up(x, y) = Up2((x, y))
 Up(x, y, z) = Up3((x, y, z))
@@ -30,5 +30,15 @@ immutable Down3{A, B, C}
     elems :: Tuple{A, B, C}
 end
 getindex(down::Down3, key) = down.elems[key]
+
++(a::Up2, b::Up2) = Up(a[1]+b[1], a[2]+b[2])
++(a::Up3, b::Up3) = Up(a[1]+b[1], a[2]+b[2], a[3]+b[3])
+-(a::Up2, b::Up2) = Up(a[1]-b[1], a[2]-b[2])
+-(a::Up3, b::Up3) = Up(a[1]-b[1], a[2]-b[2], a[3]-b[3])
+
++(a::Down2, b::Down2) = Down(a[1]+b[1], a[2]+b[2])
++(a::Down3, b::Down3) = Down(a[1]+b[1], a[2]+b[2], a[3]+b[3])
+-(a::Down2, b::Down2) = Down(a[1]-b[1], a[2]-b[2])
+-(a::Down3, b::Down3) = Down(a[1]-b[1], a[2]-b[2], a[3]-b[3])
 
 end
